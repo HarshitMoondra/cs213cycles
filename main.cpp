76896fa52069d099a63d2bfec9417f73d0afd1ad
parthshah1997 <parthshah1997@gmail.com>
2017-04-24 20:28:13 +0530

    main.cpp added

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include "cycles.h"
+#include "cycles.cpp"
+
+using namespace std;
+int main()
+{
+
+	vector<cycles> cycles;
+	while(1){
+	cout<<"Welcome to Cycle Shop \n Select an option \n Enter 1 to Sign-In \n Enter 2 to Sign-Up";
+	int n;
+	cin>>n;
+	if(n==1) 
+		{
+			signIn();
+
+		}
+
+
+	else if (n == 2) 
+		{
+			signUp();
+		}
+	}
+}
+
+
+
